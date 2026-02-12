8b009391c5121ffa5353eb91cab0cf0d930d34d0
trincohacker <trincohacker007@gmail.com>
2026-02-12 23:35:30 +0530

    Update CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -207,3 +207,164 @@ public class MonoalphabeticCipher{
 ------------------
 
 
+public class PlayfairCipher {
+
+    private static final int MATRIX_SIZE = 5;
+
+    // Function to generate the Playfair Square from the given key
+    private static char[][] generatePlayfairSquare(String key) {
+        char[][] playfairSquare = new char[MATRIX_SIZE][MATRIX_SIZE];
+        boolean[] usedLetters = new boolean[26];
+
+        // Convert the key to uppercase and remove duplicates
+        String formattedKey = key.toUpperCase().replaceAll("[^A-Z]", "").replaceAll("J", "I");
+
+        // Fill the matrix with the key
+        int row = 0, col = 0;
+        for (char ch : formattedKey.toCharArray()) {
+            if (!usedLetters[ch - 'A']) {
+                playfairSquare[row][col] = ch;
+                usedLetters[ch - 'A'] = true;
+                col++;
+                if (col == MATRIX_SIZE) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+
+        // Fill the rest of the matrix with remaining letters
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            if (ch != 'J' && !usedLetters[ch - 'A']) {
+                playfairSquare[row][col] = ch;
+                col++;
+                if (col == MATRIX_SIZE) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+
+        return playfairSquare;
+    }
+
+    // Function to find the coordinates of a given letter in the Playfair Square
+    private static void findLetterCoordinates(char[][] playfairSquare, char letter, int[] coordinates) {
+        for (int i = 0; i < MATRIX_SIZE; i++) {
+            for (int j = 0; j < MATRIX_SIZE; j++) {
+                if (playfairSquare[i][j] == letter) {
+                    coordinates[0] = i;
+                    coordinates[1] = j;
+                    return;
+                }
+            }
+        }
+    }
+
+    // Function to handle repeated letters in the plaintext
+    private static String handleRepeatedLetters(String plaintext) {
+        StringBuilder processedText = new StringBuilder(plaintext);
+        for (int i = 0; i < processedText.length() - 1; i += 2) {
+            if (processedText.charAt(i) == processedText.charAt(i + 1)) {
+                processedText.insert(i + 1, 'X');
+            }
+        }
+        return processedText.toString();
+    }
+
+    // Function to encrypt the plaintext using the Playfair Cipher
+    public static String encrypt(String plaintext, String key) {
+        char[][] playfairSquare = generatePlayfairSquare(key);
+        String processedText = handleRepeatedLetters(plaintext.toUpperCase().replaceAll("[^A-Z]", "").replaceAll("J", "I"));
+        StringBuilder ciphertext = new StringBuilder();
+
+        for (int i = 0; i < processedText.length(); i += 2) {
+            char firstChar = processedText.charAt(i);
+            char secondChar = processedText.charAt(i + 1);
+
+            int[] firstCoordinates = new int[2];
+            int[] secondCoordinates = new int[2];
+            findLetterCoordinates(playfairSquare, firstChar, firstCoordinates);
+            findLetterCoordinates(playfairSquare, secondChar, secondCoordinates);
+
+            int firstRow = firstCoordinates[0];
+            int firstCol = firstCoordinates[1];
+            int secondRow = secondCoordinates[0];
+            int secondCol = secondCoordinates[1];
+
+            char encryptedFirstChar, encryptedSecondChar;
+            if (firstRow == secondRow) {
+                encryptedFirstChar = playfairSquare[firstRow][(firstCol + 1) % MATRIX_SIZE];
+                encryptedSecondChar = playfairSquare[secondRow][(secondCol + 1) % MATRIX_SIZE];
+            } else if (firstCol == secondCol) {
+                encryptedFirstChar = playfairSquare[(firstRow + 1) % MATRIX_SIZE][firstCol];
+                encryptedSecondChar = playfairSquare[(secondRow + 1) % MATRIX_SIZE][secondCol];
+            } else {
+                encryptedFirstChar = playfairSquare[firstRow][secondCol];
+                encryptedSecondChar = playfairSquare[secondRow][firstCol];
+            }
+
+            ciphertext.append(encryptedFirstChar).append(encryptedSecondChar);
+        }
+
+        return ciphertext.toString();
+    }
+
+    // Function to decrypt the ciphertext using the Playfair Cipher
+    public static String decrypt(String ciphertext, String key) {
+        char[][] playfairSquare = generatePlayfairSquare(key);
+        StringBuilder plaintext = new StringBuilder();
+
+        for (int i = 0; i < ciphertext.length(); i += 2) {
+            char firstChar = ciphertext.charAt(i);
+            char secondChar = ciphertext.charAt(i + 1);
+
+            int[] firstCoordinates = new int[2];
+            int[] secondCoordinates = new int[2];
+            findLetterCoordinates(playfairSquare, firstChar, firstCoordinates);
+            findLetterCoordinates(playfairSquare, secondChar, secondCoordinates);
+
+            int firstRow = firstCoordinates[0];
+            int firstCol = firstCoordinates[1];
+            int secondRow = secondCoordinates[0];
+            int secondCol = secondCoordinates[1];
+
+            char decryptedFirstChar, decryptedSecondChar;
+            if (firstRow == secondRow) {
+                decryptedFirstChar = playfairSquare[firstRow][(firstCol - 1 + MATRIX_SIZE) % MATRIX_SIZE];
+                decryptedSecondChar = playfairSquare[secondRow][(secondCol - 1 + MATRIX_SIZE) % MATRIX_SIZE];
+            } else if (firstCol == secondCol) {
+                decryptedFirstChar = playfairSquare[(firstRow - 1 + MATRIX_SIZE) % MATRIX_SIZE][firstCol];
+                decryptedSecondChar = playfairSquare[(secondRow - 1 + MATRIX_SIZE) % MATRIX_SIZE][secondCol];
+            } else {
+                decryptedFirstChar = playfairSquare[firstRow][secondCol];
+                decryptedSecondChar = playfairSquare[secondRow][firstCol];
+            }
+
+            plaintext.append(decryptedFirstChar).append(decryptedSecondChar);
+        }
+
+        return plaintext.toString();
+    }
+
+    public static void main(String[] args) {
+        String plaintext = "HELLO";
+        String key = "KEYWORD";
+
+        String encryptedText = encrypt(plaintext, key);
+        String decryptedText = decrypt(encryptedText, key);
+
+        System.out.println("Plaintext: " + plaintext);
+        System.out.println("Key:       " + key);
+        System.out.println("Encrypted: " + encryptedText);
+        System.out.println("Decrypted: " + decryptedText);
+    }
+}
+
+
+
+
+----------
+
+
+
