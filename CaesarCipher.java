11ee80846e20878d57003c3d155a3cd158f75d6a
trincohacker <trincohacker007@gmail.com>
2026-02-12 23:39:29 +0530

    Update CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -676,4 +676,40 @@ public class RailFenceCipher{
 ----------
 
 
+import java.util.*;
+public class oneTimePadCipher{
+	
+	public static String encrypt(String plaintext, String key){
+		StringBuilder ciphertext = new StringBuilder();
+		for(int i=0;i<plaintext.length();i++){
+			char ch = plaintext.charAt(i);
+			char encryptedchar = (char) (ch ^ key.charAt(i));
+			ciphertext.append(encryptedchar);
+		}
+		return ciphertext.toString();
+	}
+	
+	public static String decrypt(String ciphertext, String key){
+		return encrypt(ciphertext,key);
+	}
+	
+	public static void main(String[] args){
+		Scanner sc =  new Scanner(System.in);
+		String plaintext = "HELLOWORLD";
+		String key = "RANDOM1234";
+		
+		if(plaintext.length() != key.length()){
+			System.out.println("The plaintext and the key should be same length");
+		}
+		
+		String encryptedText = encrypt(plaintext,key);
+		String decryptedText = decrypt(encryptedText,key);
+		
+		System.out.println("Plaintext:"+plaintext);
+		System.out.println("Encrypted Text:"+encryptedText);
+		System.out.println("Decrypted Text:"+decryptedText);
+	}
+}
+
+
 
