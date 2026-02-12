9c4443b3e9d3e895d63c091067ec37d86ad35714
trincohacker <trincohacker007@gmail.com>
2026-02-12 23:36:10 +0530

    Update CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -367,4 +367,64 @@ public class PlayfairCipher {
 ----------
 
 
+public class PolyalphabeticCipher {
+
+    // Function to encrypt the plaintext using the Polyalphabetic Cipher
+    public static String encrypt(String plaintext, String keyword) {
+        StringBuilder ciphertext = new StringBuilder();
+        plaintext = plaintext.toUpperCase();
+        keyword = keyword.toUpperCase();
+
+        int keywordIndex = 0;
+        for (char ch : plaintext.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                int shift = keyword.charAt(keywordIndex) - 'A';
+                char encryptedChar = (char) ((ch - 'A' + shift) % 26 + 'A');
+                ciphertext.append(encryptedChar);
+                keywordIndex = (keywordIndex + 1) % keyword.length();
+            } else {
+                ciphertext.append(ch);
+            }
+        }
+        return ciphertext.toString();
+    }
+    // Function to decrypt the ciphertext using the Polyalphabetic Cipher
+    public static String decrypt(String ciphertext, String keyword) {
+        StringBuilder plaintext = new StringBuilder();
+        ciphertext = ciphertext.toUpperCase();
+        keyword = keyword.toUpperCase();
+
+        int keywordIndex = 0;
+        for (char ch : ciphertext.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                int shift = keyword.charAt(keywordIndex) - 'A';
+                char decryptedChar = (char) ((ch - 'A' - shift + 26) % 26 + 'A');
+                plaintext.append(decryptedChar);
+
+                keywordIndex = (keywordIndex + 1) % keyword.length();
+            } else {
+                plaintext.append(ch);
+            }
+        }
+
+        return plaintext.toString();
+    }
+
+    public static void main(String[] args) {
+        String plaintext = "HELLO";
+        String keyword = "KEY";
+
+        String encryptedText = encrypt(plaintext, keyword);
+        String decryptedText = decrypt(encryptedText, keyword);
+
+        System.out.println("Plaintext: " + plaintext);
+        System.out.println("Keyword: " + keyword);
+        System.out.println("Encrypted: " + encryptedText);
+        System.out.println("Decrypted: " + decryptedText);
+    }
+}
+
+
+---------
+
 
